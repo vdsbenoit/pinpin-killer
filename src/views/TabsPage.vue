aa8eeae65919a9c516db67b5a1288a602d0baed8
vdsbenoit <vdsbenoit@gmail.com>
2023-08-11 04:30:28 +0200

    Hide tab3 for now

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -13,10 +13,10 @@
           <ion-label>PVs</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
+        <!-- <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon aria-hidden="true" :icon="podium" />
           <ion-label>Classement</ion-label>
-        </ion-tab-button>
+        </ion-tab-button> -->
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
